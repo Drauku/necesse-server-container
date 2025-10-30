d8ec1735b4bcbf344a2beec63c73f7b03ccf6c89
Drauku <drauku@gmail.com>
2025-10-30 00:56:36 -0500

    revert java cmd changes to test rebuild

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,46 +22,46 @@ CFG_FILE="$CFG_DIR/server.cfg"
 echo -e "DEBUG: CFG_FILE: $CFG_FILE"
 
 # Create default server.cfg if missing
-# if [ ! -f "$CFG_FILE" ]; then
-#     cat > "$CFG_FILE" <<'EOF'
-# SERVER = {
-#     port = 14159,
-#     slots = 10,
-#     password = ,
-#     maxClientLatencySeconds = 30,
-#     pauseWhenEmpty = true,
-#     strictServerAuthority = false,
-#     logging = true,
-#     language = en,
-#     unloadLevelsCooldown = 30,
-#     droppedItemsLifeMinutes = 0,
-#     unloadSettlements = false,
-#     maxSettlementsPerPlayer = -1,
-#     maxSettlersPerSettlement = -1,
-#     zipSaves = true,
-#     MOTD =
-# }
-# EOF
-# fi
+if [ ! -f "$CFG_FILE" ]; then
+    cat > "$CFG_FILE" <<'EOF'
+SERVER = {
+    port = 14159,
+    slots = 10,
+    password = ,
+    maxClientLatencySeconds = 30,
+    pauseWhenEmpty = true,
+    strictServerAuthority = false,
+    logging = true,
+    language = en,
+    unloadLevelsCooldown = 30,
+    droppedItemsLifeMinutes = 0,
+    unloadSettlements = false,
+    maxSettlementsPerPlayer = -1,
+    maxSettlersPerSettlement = -1,
+    zipSaves = true,
+    MOTD =
+}
+EOF
+fi
 
-# write_cfg() {
-#   key="$1" value="$2"
-#   # Remove old line
-#   sed -i "/^[[:space:]]*${key}[[:space:]]*=/d" "$CFG_FILE" 2>/dev/null || true
-#   # Append
-#   printf "%s=%s\n" "$key" "$value" >> "$CFG_FILE"
-# }
+write_cfg() {
+  key="$1" value="$2"
+  # Remove old line
+  sed -i "/^[[:space:]]*${key}[[:space:]]*=/d" "$CFG_FILE" 2>/dev/null || true
+  # Append
+  printf "%s=%s\n" "$key" "$value" >> "$CFG_FILE"
+}
 
 # Apply ENV vars (non-empty world required!)
-# write_cfg "world"               "${WORLD:-necesseworld}"
-# write_cfg "slots"               "${SLOTS:-10}"
-# write_cfg "owner"               "${OWNER:-}"
-# write_cfg "password"            "${PASSWORD:-}"
-# write_cfg "motd"                "${MOTD:-This server made possible by Docker!}"
-# write_cfg "pause"               "${PAUSE:-0}"
-# write_cfg "giveclientspower"    "${GIVE_CLIENTS_POWER:-1}"
-# write_cfg "logging"             "${LOGGING:-1}"
-# write_cfg "zip"                 "${ZIP:-1}"
+write_cfg "world"               "${WORLD:-necesseworld}"
+write_cfg "slots"               "${SLOTS:-10}"
+write_cfg "owner"               "${OWNER:-}"
+write_cfg "password"            "${PASSWORD:-}"
+write_cfg "motd"                "${MOTD:-This server made possible by Docker!}"
+write_cfg "pause"               "${PAUSE:-0}"
+write_cfg "giveclientspower"    "${GIVE_CLIENTS_POWER:-1}"
+write_cfg "logging"             "${LOGGING:-1}"
+write_cfg "zip"                 "${ZIP:-1}"
 
 ENV WORLD="necesseworld"
 ENV SLOTS="10"
@@ -77,16 +77,16 @@ JAVA_CMD="java"
 [ -n "${JVMARGS:-}" ] && JAVA_CMD="${JAVA_CMD} ${JVMARGS}"
 
 echo "Starting Necesse server..."
-# exec $JAVA_CMD -jar "${GAME_DIR}/server/Server.jar" -nogui
-exec $JAVA_CMD ${JVMARGS} \
-  -jar Server.jar \
-  -nogui -localdir \
-  -world "${WORLD}" \
-  -slots "${SLOTS}" \
-  -owner "${OWNER}" \
-  -motd "${MOTD}" \
-  -password "${PASSWORD}" \
-  -pausewhenempty "${PAUSE}" \
-  -giveclientspower "${GIVE_CLIENTS_POWER}" \
-  -logging "${LOGGING}" \
-  -zipsaves "${ZIP}"
+exec $JAVA_CMD -jar "${GAME_DIR}/server/Server.jar" -nogui
+# exec $JAVA_CMD ${JVMARGS} \
+#   -jar Server.jar \
+#   -nogui -localdir \
+#   -world "${WORLD}" \
+#   -slots "${SLOTS}" \
+#   -owner "${OWNER}" \
+#   -motd "${MOTD}" \
+#   -password "${PASSWORD}" \
+#   -pausewhenempty "${PAUSE}" \
+#   -giveclientspower "${GIVE_CLIENTS_POWER}" \
+#   -logging "${LOGGING}" \
+#   -zipsaves "${ZIP}"
